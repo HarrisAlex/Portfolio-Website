fb23fdc842ef4b31a1a460afc89ed0438c07f981
Alex Harris <72664735+HarrisAlex@users.noreply.github.com>
2023-10-24 13:07:29 -0400

    Unified project data

---
+++ javascript/projects-data.js
@@ -0,0 +1,25 @@
+import { Project } from "./modules/project-card.js";
+
+export var cityAlleyProject = new Project(
+    "City Alley", 
+    "A dark, quiet alley", 
+    "I created this 3D rendering of an alley using Blender. My goal for this project was achieving photorealism using a combination of lighting and texturing.",
+    "projects/city-alley.html",
+    "images/projects/city-alley-preview.webp"
+);
+
+export var creepyHotelHallway = new Project(
+    "Creepy Hotel Hallway", 
+    "A creepy, dark hotel hallway", 
+    "I created this simple 3D rendering a creepy hotel hallway using Blender as a fun, small project to practice my skills.",
+    "projects/creepy-hallway.html",
+    "images/projects/creepy-hotel-hallway-preview.webp"
+);
+
+export var coffeeShopWebsite = new Project(
+    "Coffee Shop Website",
+    "A mock-up for a cafe",
+    "I created this mock-up for a coffee shop website using HTML and CSS. I wanted to practice my skills with Flexbox and responsive design.",
+    "projects/coffee-shop-website.html",
+    "images/projects/coffee-shop-website-preview.webp"
+);
--- javascript/projects.js
+++ javascript/projects.js
@@ -1,28 +1,5 @@
-import { Project, CreateProjectCard } from "./modules/project-card.js";
-
-var cityAlleyProject = new Project(
-    "City Alley", 
-    "A dark, quiet alley", 
-    "I created this 3D rendering of an alley using Blender. My goal for this project was achieving photorealism using a combination of lighting and texturing.",
-    "projects/city-alley.html",
-    "images/projects/city-alley-preview.webp"
-);
-
-var creepyHotelHallway = new Project(
-    "Creepy Hotel Hallway", 
-    "A creepy, dark hotel hallway", 
-    "I created this simple 3D rendering a creepy hotel hallway using Blender as a fun, small project to practice my skills.",
-    "projects/creepy-hallway.html",
-    "images/projects/creepy-hotel-hallway-preview.webp"
-);
-
-var coffeeShopWebsite = new Project(
-    "Coffee Shop Website",
-    "A mock-up for a cafe",
-    "I created this mock-up for a coffee shop website using HTML and CSS. I wanted to practice my skills with Flexbox and responsive design.",
-    "projects/coffee-shop-website.html",
-    "images/projects/coffee-shop-website-preview.webp"
-);
+import { CreateProjectCard } from "./modules/project-card.js";
+import {cityAlleyProject, creepyHotelHallway, coffeeShopWebsite} from './projects-data.js';
 
 var container = $("#projects-container");
 
