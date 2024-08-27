243cdb9bb6a019cbce0b58eb0a5c8bc6a5d40feb
Alex Harris <72664735+HarrisAlex@users.noreply.github.com>
2024-08-27 14:34:35 -0400

    Fixed prospect backend

--- prospect.php
+++ prospect.php
@@ -13,6 +13,28 @@
         sanitize_input($description);
 
         if (!empty($name) && !empty($company) && !empty($email) && !empty($description)) {
+            $message = "
+                <html>
+                    <head>
+                        <title>Message from {$name}</title>
+                    </head>
+                    <body>
+                        <h2>Message from: {$name}</h2>
+                        <p>{$name} has reached out about a project, representing {$company}. They supplied {$email} as a way to contact them.</p>
+                        <br>
+                        <p>Here's what they need:</p>
+                        <br>
+                        <p>{$description}</p>
+                    </body>
+                </html>
+            ";
+
+            $headers[] = 'MIME-Version: 1.0';
+            $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+            $headers[] = 'From: Personal Website <alexharris.design>';
+
+            mail(getenv('EMAIL'), "Personal Website Prospect", $message, implode("\r\n", $headers));
+
             echo 200;
         } else {
             echo 400;
